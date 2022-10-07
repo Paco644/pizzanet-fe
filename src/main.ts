d13a578956854c406fb029fd4f67b551b11e86bb
Paco644 <p.scharnetzki@outlook.de>
2022-10-07 13:51:38 +0200

    Added Vuetify and i18n

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,33 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+import { createI18n } from 'vue-i18n'
 
-createApp(App).use(router).mount('#app')
+// const messages = {
+//   enUS: {
+//     message: {
+//       hello: 'Hello World!'
+//     }
+//   },
+//   deDE: {
+//     message: {
+//       hello: 'Hallo Welt!'
+//     }
+//   }
+// }
+
+// const i18n = createI18n({
+//   locale: 'en',
+//   fallbackLocale: 'de',
+//   allowComposition: true,
+// })
+
+loadFonts()
+
+createApp(App).use(i18n)
+  .use(router)
+  .use(i18n)
+  .use(vuetify)
+  .mount('#app')
